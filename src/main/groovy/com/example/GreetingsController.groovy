4c0e04c64f4b58d1cc5bce239a718da13cd70440
Daniel Kreuter <daniel.kreuter85@gmail.com>
2017-02-17 19:38:19 +0100

    Initial commit

---
+++ src/main/groovy/com/example/GreetingsController.groovy
@@ -0,0 +1,10 @@
+package com.example
+
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * Created by dkreuter on 17.02.17.
+ */
+@RestController
+class GreetingsController {
+}
---
+++ src/main/groovy/com/example/JerseyConfiguration.groovy
@@ -0,0 +1,15 @@
+package com.example
+
+import org.glassfish.jersey.server.ResourceConfig
+import org.springframework.context.annotation.Configuration
+
+/**
+ * Created by dkreuter on 17.02.17.
+ */
+@Configuration
+class JerseyConfiguration extends ResourceConfig {
+
+    public JerseyConfiguration() {
+
+    }
+}
